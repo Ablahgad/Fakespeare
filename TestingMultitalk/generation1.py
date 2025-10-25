f05fdd995c67a6bb1814f5b811719d594db6ece8
Ablah <115513366+Ablahgad@users.noreply.github.com>
2025-10-25 17:29:45 -0400

    More experimenting with having several voices and background noise

--- TestingMultitalk/generation1.py
+++ TestingMultitalk/generation1.py
@@ -3,6 +3,7 @@ import base64
 import os
 import wave
 import click
+import re
 
 BOSON_API_KEY = os.getenv("BOSON_API_KEY")
 
@@ -12,26 +13,25 @@ MULTISPEAKER_DEFAULT_SYSTEM_MESSAGE = """You are an AI assistant designed to con
 If the user's message includes a [SPEAKER*] tag, do not read out the tag and generate speech for the following text, using the specified voice.
 If no speaker tag is present, select a suitable voice on your own."""
 
-@click.command()
-@click.option(
-    "--transcript",
-    type=str,
-    default= r"C:\Users\ablah\repos\SAS\TestingMultitalk\tomorrow.txt",
-    help="The prompt to use for generation. If not set, we will use a default prompt.",
-)
-@click.option(
-    "--scene_prompt",
-    type=str,
-    default=f"./quiet_indoor.txt",
-    help="The scene description prompt to use for generation. If not set, or set to `empty`, we will leave it to empty.",
-)
-
+# @click.command()
+# @click.option(
+#     "--transcript",
+#     type=str,
+#     default= r"C:\Users\ablah\repos\SAS\TestingMultitalk\tomorrow.txt",
+#     help="The prompt to use for generation. If not set, we will use a default prompt.",
+# )
+# @click.option(
+#     "--scene_prompt",
+#     type=str,
+#     default=f"./quiet_indoor.txt",
+#     help="The scene description prompt to use for generation. If not set, or set to `empty`, we will leave it to empty.",
+# )
 def b64(path):
     return base64.b64encode(open(path, "rb").read()).decode("utf-8")
 
-def main(transcript, scene_prompt):
-    if os.path.exists(transcript):
-        with open(transcript, "r", encoding="utf-8") as f:
+def main():
+    if os.path.exists(r"C:\Users\ablah\repos\SAS\TestingMultitalk\tomorrow.txt"):
+        with open(r"C:\Users\ablah\repos\SAS\TestingMultitalk\tomorrow.txt", "r", encoding="utf-8") as f:
             transcript = f.read().strip()
 
     # if len(speaker_tags) > 1:
@@ -89,7 +89,11 @@ def main(transcript, scene_prompt):
         ("[cough]", "<SE>[Cough]</SE>"),
     ]:
         transcript = transcript.replace(tag, replacement)
+
+    transcript = transcript.replace("[", "<|speaker_id_start|>")
+    transcript = transcript.replace("]", "<|speaker_id_end|>")
     lines = transcript.split("\n")
+
     transcript = "\n".join([" ".join(line.split()) for line in lines if line.strip()])
     transcript = transcript.strip()
 
@@ -103,7 +107,15 @@ def main(transcript, scene_prompt):
             
             {"role": "system", "content": "You are an AI assistant designed to convert text into speech. If the user's message includes a [SPEAKER*] tag, do not read out the tag and generate speech for the following text, using the specified voice. If no speaker tag is present, select a suitable voice on your own."},
             {"role": "user", "content": transcript},
-            {"role": "system", "content": f"Generate audio following instruction: \n\n<|scene_desc_start|>\n It is AUDIBLY raining \n SPEAKER1: masculine\nSPEAKER2: feminine \n <|scene_desc_end|>"},
+            {"role": "system", "content": f"""Generate realistic multi-speaker audio.
+             
+                <|scene_desc_start|>
+                It is raining heavily, and thunder rolls in the distance.
+                <|scene_desc_end|>
+
+                <|speaker_id_start|>SPEAKER1<|speaker_id_end|> masculine voice
+                <|speaker_id_start|>SPEAKER2<|speaker_id_end|> feminine voice
+                """}
         ],
         modalities=["text", "audio"],
         max_completion_tokens=4096,
@@ -118,4 +130,4 @@ def main(transcript, scene_prompt):
     open("output.wav", "wb").write(base64.b64decode(audio_b64))
 
 if __name__ == "__main__":
-    main(r"C:\Users\ablah\repos\SAS\TestingMultitalk\tomorrow.txt","./quiet_indoor.txt")
+    main()
